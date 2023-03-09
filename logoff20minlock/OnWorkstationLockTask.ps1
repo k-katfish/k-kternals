4896bb2d66db20d322c1da628818486d34b7e5fb
katfish <kkatfish@cs.colostate.edu>
2023-03-08 18:00:37 -0700

    updated owl task to actually work

--- logoff20minlock/OnWorkstationLockTask.ps1
+++ logoff20minlock/OnWorkstationLockTask.ps1
@@ -1,5 +1,6 @@
 [Environment]::SetEnvironmentVariable("OWLpid", $PID, "User")
+$SessionID = (Get-Process -PID $PID).SessionId
 
 Start-Sleep -Seconds 1200
 
-& "C:\Windows\System32\shutdown.exe" /l
+logoff.exe $SessionID
