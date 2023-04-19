65f6b30deca84d772eb632835120914df1748286
katfish <kkatfish@cs.colostate.edu>
2023-04-19 08:14:56 -0600

    Deleted old autologoff tool that was hacky and weird

--- logoff20minlock/OnWorkstationLockTask.ps1
+++
@@ -1,8 +0,0 @@
-if ($env:USERNAME -eq "Administrator") { exit }
-
-[Environment]::SetEnvironmentVariable("OWLpid", $PID, "User")
-$SessionID = (Get-Process -PID $PID).SessionId
-
-Start-Sleep -Seconds 1200
-
-logoff.exe $SessionID
--- logoff20minlock/OnWorkstationUnlockTask.ps1
+++
@@ -1,7 +0,0 @@
-if ($env:USERNAME -eq "Administrator") { exit }
-
-$OWLpid = [Environment]::GetEnvironmentVariable("OWLpid", "User")
-
-Stop-Process -Id $OWLpid -Force
-
-[Environment]::SetEnvironmentVariable("OWLpid", $null, "User")
