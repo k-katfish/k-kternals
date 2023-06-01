0a37cd714a6c3a11126fc9648cd1d0e086dc0b4a
katfish <kkatfish@cs.colostate.edu>
2023-06-01 10:56:21 -0600

    Added Scanner Launcher for Localhost

---
+++ Scan/LaunchScannerLocalhost.ps1
@@ -0,0 +1,21 @@
+Write-Host "  ____   ____    _    _   _  "
+Write-Host " / ___| / ___|  / \  | \ | | "
+Write-Host " \___ \| |     / _ \ |  \| | "
+Write-Host "  ___) | |___ / ___ \| |\  | "
+Write-Host " |____/ \____/_/   \_\_| \_| "
+Write-Host "                             "
+
+Write-Host "Written by Kyle Ketchell for Engineering Technology Services"
+Write-Host " LOCALHOST VERSION: SCANS 'THIS' COMPUTER"
+
+#$ComputerName = Read-Host -Prompt "Enter Computer Name to scan"
+$ComputerName = $env:COMPUTERNAME
+#$ComputerName = $ComputerName.Substring(0,$ComputerName.Length-1)
+Write-Host "Scanning This Computer' : '$ComputerName' now, this window will close in a few seconds."
+
+$Location = Get-Location
+#Write-Host "executing: Powershell -Command {& `"$($Location.Path)\ScanHost.ps1`" -Hostname $($ComputerName)}"
+
+Start-Process Powershell.exe -ArgumentList "Powershell $($Location.Path)\ScanHost.ps1 -Hostname $($ComputerName)" -WindowStyle:Hidden
+
+timeout 5 /NOBREAK
